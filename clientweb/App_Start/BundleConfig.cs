98eb1b4b87c880435b2fe26f5c3ca5c6380c4566
Tuan Bui <kingtuan8@gmail.com>
2020-01-02 11:46:35 +0700

    Add project files.

---
+++ clientweb/App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace clientweb
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                 "~/Scripts/bootstrap.js", "~/Scripts/owl.carousel.min.js", "~/Scripts/jquery.sticky.js", "~/Scripts/jquery.waypoints.min.js",
+                  "~/Scripts/aos.js",
+            "~/Scripts/main.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                       "~/Content/style.css",
+                        "~/Content/aos.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ clientweb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="clientweb.MvcApplication" Language="C#" %>
