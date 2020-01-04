31865d9aabfeb867c5e57291ee05f36af2415867
tuanb <tuanb@NOME>
2020-01-04 22:35:39 +0700

    f

--- clientweb/Controllers/HomeController.cs
+++ clientweb/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Security.Cryptography;
+using System.Text;
 using System.Web.Mvc;
 
 namespace clientweb.Controllers
@@ -31,5 +29,26 @@ namespace clientweb.Controllers
         {
             return View();
         }
+
+        public JsonResult  Login(string username, string password)
+        {
+            string flag = "OK";
+
+
+            return Json(flag, JsonRequestBehavior.AllowGet);
+        }
+
+        public static string GetMD5Hash(string value)
+        {
+            MD5 md5Hasher = MD5.Create();
+            byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(value));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+
     }
 }
